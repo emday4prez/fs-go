50e70d5f03ab4a72d157c7b20ac951c5c95704b0
Emerson <emerson.day@gmail.com>
2025-06-21 13:13:16 -0700

    using slog

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
-	"log"
 	"log/slog"
 	"net/http"
 	"strings"
@@ -34,13 +33,13 @@ func (s *Server) WelcomeHandler(w http.ResponseWriter, r *http.Request) {
 func (s *Server) ShowUploadPage(w http.ResponseWriter, r *http.Request) {
 	tmp, err := template.ParseFiles("web/template/upload.html")
 	if err != nil {
-		log.Printf("Error parsing template: %v", err)
+		s.logger.Error("Could not parse template", "path", "web/template/list.html", "error", err)
 		http.Error(w, "There was a problem preparing the page.", http.StatusInternalServerError)
 		return
 	}
 	err = tmp.Execute(w, nil)
 	if err != nil {
-		log.Printf("error executing template: %v", err)
+		s.logger.Error("Could not execute template", "error", err)
 		http.Error(w, "there was a problem rendering the page", http.StatusInternalServerError)
 	}
 }
@@ -49,6 +48,7 @@ func (s *Server) UploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	//parse headers, max size 10mb
 	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		s.logger.Error("file too big to upload", "error", err)
 		http.Error(w, "file too big, max 10MB", http.StatusBadRequest)
 		return
 	}
@@ -56,7 +56,7 @@ func (s *Server) UploadHandler(w http.ResponseWriter, r *http.Request) {
 	// get file from 'name' html attribute
 	_, fileHeader, err := r.FormFile("file")
 	if err != nil {
-		log.Printf("Error retrieving file from: %v", err)
+		s.logger.Error("error retrieving file", "error", err)
 		http.Error(w, "error retrieving the file", http.StatusInternalServerError)
 		return
 	}
@@ -64,7 +64,7 @@ func (s *Server) UploadHandler(w http.ResponseWriter, r *http.Request) {
 	// call service
 	err = s.fileService.SaveFile(fileHeader)
 	if err != nil {
-		log.Printf("error saving file: %v", err)
+		s.logger.Error("error saving file", "error", err)
 		http.Error(w, "error saving the file", http.StatusInternalServerError)
 		return
 	}
@@ -75,20 +75,20 @@ func (s *Server) UploadHandler(w http.ResponseWriter, r *http.Request) {
 func (s *Server) ShowListPage(w http.ResponseWriter, r *http.Request) {
 	filenames, err := s.fileService.ListFiles()
 	if err != nil {
-		log.Printf("Error listing files: %v", err)
+		s.logger.Error("Could not retrieve file list", "error", err)
 		http.Error(w, "Could not retrieve file list.", http.StatusInternalServerError)
 		return
 	}
 
 	tmp, err := template.ParseFiles("web/template/list.html")
 	if err != nil {
-		log.Printf("Error parsing template: %v", err)
+		s.logger.Error("Could not parse template", "path", "web/template/list.html", "error", err)
 		http.Error(w, "There was a problem preparing the page.", http.StatusInternalServerError)
 		return
 	}
 	err = tmp.Execute(w, filenames)
 	if err != nil {
-		log.Printf("error executing template: %v", err)
+		s.logger.Error("error executing template", "error", err)
 		http.Error(w, "there was a problem rendering the page", http.StatusInternalServerError)
 	}
 }
@@ -102,7 +102,7 @@ func (s *Server) DownloadHandler(w http.ResponseWriter, r *http.Request) {
 
 	_, filePath, err := s.fileService.GetFile(filename)
 	if err != nil {
-		log.Printf("Error getting file: %v", err)
+		s.logger.Error("error getting file", "error", err)
 		http.Error(w, "File not found.", http.StatusNotFound)
 		return
 	}
@@ -113,7 +113,7 @@ func (s *Server) DownloadHandler(w http.ResponseWriter, r *http.Request) {
 func (s *Server) ListFilesAPI(w http.ResponseWriter, r *http.Request) {
 	filenames, err := s.fileService.ListFiles()
 	if err != nil {
-		log.Printf("Error listing files for API: %v", err)
+		s.logger.Error("error listing files for api", "error", err)
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
 
@@ -125,6 +125,6 @@ func (s *Server) ListFilesAPI(w http.ResponseWriter, r *http.Request) {
 	err = json.NewEncoder(w).Encode(filenames)
 	if err != nil {
 
-		log.Printf("Error encoding files to JSON: %v", err)
+		s.logger.Error("error encoding json", "error", err)
 	}
 }
