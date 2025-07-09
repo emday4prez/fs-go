f2ecd8ff61dca78d66d61d9be20bba4accb20047
Emerson <emerson.day@gmail.com>
2025-07-08 20:46:45 -0700

    begin domain

---
+++ internal/domain/user.go
@@ -0,0 +1,24 @@
+package domain
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID             string
+	Username       string
+	HashedPassword string
+}
+
+func NewUser(id, username, password string) (*User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		ID:             id,
+		Username:       username,
+		HashedPassword: string(hashedPassword),
+	}, nil
+}
