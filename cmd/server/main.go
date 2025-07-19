97a40be451a4ed269296dbc1c37040201e3275d1
Emerson <emerson.day@gmail.com>
2025-07-18 21:06:18 -0700

    update main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/emday4prez/fs-go/internal/api"
 	"github.com/emday4prez/fs-go/internal/config"
 	"github.com/emday4prez/fs-go/internal/file"
+	"github.com/emday4prez/fs-go/internal/user"
 )
 
 func main() {
@@ -20,9 +21,12 @@ func main() {
 		logger.Error("Failed to create local storage", "error", err)
 		os.Exit(1)
 	}
+
 	fileService := file.NewFileService(localStorage)
 
-	router := api.NewRouter(fileService, cfg, logger)
+	inMemoryStorage := user.NewInMemoryStorage()
+	userService := user.NewService(inMemoryStorage)
+	router := api.NewRouter(fileService, userService, cfg, logger)
 
 	logger.Info("Server is starting", "port", cfg.Port)
 	if err := http.ListenAndServe(cfg.Port, router); err != nil {
